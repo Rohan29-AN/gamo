34400751d273af16cbbdec7e955cc3ad880e82c7
Rohan <rohanrc101@gmail.com>
2024-12-27 21:27:53 +0300

    [FEAT] translate the contents of the page (to be continued)

--- transations.js
+++ transations.js
@@ -11,6 +11,9 @@ function loadTranslations(language) {
 
 function updatePageContent() {
     //TODO: Update all page elements with the translated content
+    document.title = translations.title
+    var setupTitle = document.querySelector(".setup-title");
+    setupTitle.innerHTML = translations.step1
 }
 
 function changeLanguage() {
