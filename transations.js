3e91186fecc89cda09de8f97ea068709b5170d30
Rohan <rohanrc101@gmail.com>
2024-12-27 21:04:54 +0300

    [FEAT] create functions to manage translation logics

--- transations.js
+++ transations.js
@@ -1,1 +1,24 @@
-console.log("Document title :"+ document.title)
+let translations = {};
+
+function loadTranslations(language) {
+    fetch(`./locales/${language}.json`)
+        .then(response => response.json())
+        .then(data => {
+            translations = data;
+            updatePageContent();
+        });
+}
+
+function updatePageContent() {
+    //TODO: Update all page elements with the translated content
+}
+
+function changeLanguage() {
+    const language = document.getElementById('language-selector').value;
+    loadTranslations(language);
+}
+
+// Load default language (e.g., French) on page load
+window.onload = function () {
+    loadTranslations('fr');
+};
